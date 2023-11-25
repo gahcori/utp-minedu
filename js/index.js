b3185163b48e5ee0f2b64857d582205a85b1d5fa
Guido Alvaro <32649659+gahcori@users.noreply.github.com>
2023-11-25 12:14:56 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,142 +1,128 @@
-! function (e) {
-  var t = {};
+! function () {
+    "use strict";
+    var e = [, function (e, t, n) {
+            function r(e, t) {
+                const n = document;
+                n.addEventListener("click", (r => {
+                    (r.target.matches(e) || r.target.matches(`${e} *`)) && (n.querySelector(t).classList.toggle("active"), n.querySelector(e).classList.toggle("is-active"))
+                }))
+            }
+            n.r(t), n.d(t, {
+                default: function () {
+                    return r
+                }
+            })
+        }, function (e, t, n) {
+            function r(e, t) {
+                window.scrollY >= t ? e.classList.add("sticky") : e.classList.remove("sticky")
+            }
+            n.r(t), n.d(t, {
+                menuSticky: function () {
+                    return r
+                }
+            })
+        }, function (e, t, n) {
+            function r() {
+                const e = document,
+                    t = e.getElementById("slider"),
+                    n = e.querySelector(".slider-prev"),
+                    r = e.querySelector(".slider-next"),
+                    i = e.querySelectorAll(".slider-item");
+                if (!t) return;
+                let c = 0,
+                    a = i.length;
 
-  function n(r) {
-    if (t[r]) return t[r].exports;
-    var c = t[r] = {
-      i: r,
-      l: !1,
-      exports: {}
-    };
-    return e[r].call(c.exports, c, c.exports, n), c.l = !0, c.exports
-  }
-  n.m = e, n.c = t, n.d = function (e, t, r) {
-    n.o(e, t) || Object.defineProperty(e, t, {
-      enumerable: !0,
-      get: r
-    })
-  }, n.r = function (e) {
-    "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
-      value: "Module"
-    }), Object.defineProperty(e, "__esModule", {
-      value: !0
-    })
-  }, n.t = function (e, t) {
-    if (1 & t && (e = n(e)), 8 & t) return e;
-    if (4 & t && "object" == typeof e && e && e.__esModule) return e;
-    var r = Object.create(null);
-    if (n.r(r), Object.defineProperty(r, "default", {
-        enumerable: !0,
-        value: e
-      }), 2 & t && "string" != typeof e)
-      for (var c in e) n.d(r, c, function (t) {
-        return e[t]
-      }.bind(null, c));
-    return r
-  }, n.n = function (e) {
-    var t = e && e.__esModule ? function () {
-      return e.default
-    } : function () {
-      return e
-    };
-    return n.d(t, "a", t), t
-  }, n.o = function (e, t) {
-    return Object.prototype.hasOwnProperty.call(e, t)
-  }, n.p = "", n(n.s = 0)
-}([function (e, t, n) {
-  "use strict";
-  n.r(t);
-  var r = n(1),
-    c = n(2),
-    i = n(3),
-    o = n(4);
-  const l = document,
-    a = window,
-    s = l.getElementById("nav-main"),
-    u = s.offsetTop;
-  l.addEventListener("DOMContentLoaded", e => {
-    Object(r.default)(".icon-menu", ".menu"), Object(i.slider)(), Object(o.view_pdf)()
-  }), a.addEventListener("scroll", e => {
-    Object(c.menuSticky)(s, u)
-  })
-}, function (e, t, n) {
-  "use strict";
+                function s() {
+                    i[c].classList.remove("active"), c++, c >= a && (c = 0), i[c].classList.add("active")
+                }
+                e.addEventListener("click", (e => {
+                    e.target === n && (e.preventDefault(), i[c].classList.remove("active"), c--, c < 0 && (c = a - 1), i[c].classList.add("active")), e.target === r && (e.preventDefault(), s())
+                })), setInterval((() => s()), 5e3)
+            }
+            n.r(t), n.d(t, {
+                slider: function () {
+                    return r
+                }
+            })
+        }, function (e, t, n) {
+            n.r(t), n.d(t, {
+                view_pdf: function () {
+                    return c
+                }
+            });
+            const r = document,
+                i = r.getElementById("req");
 
-  function r(e, t) {
-    const n = document;
-    n.addEventListener("click", r => {
-      (r.target.matches(e) || r.target.matches("".concat(e, " *"))) && (n.querySelector(t).classList.toggle("active"), n.querySelector(e).classList.toggle("is-active"))
-    })
-  }
-  n.r(t), n.d(t, "default", (function () {
-    return r
-  }))
-}, function (e, t, n) {
-  "use strict";
+            function c() {
+                if (!i) return;
+                let e = window.matchMedia("(min-width: 600px)");
+                r.addEventListener("click", (t => {
+                    const n = t.target,
+                        i = r.getElementById("file-view"),
+                        c = r.getElementById("file-view2"),
+                        a = r.getElementById("file-view3"),
+                        s = r.getElementById("file-view4");
+                    if ("card_req-link" === n.className) {
+                        e.matches && t.preventDefault();
+                        const r = n.href;
+                        i.data = `${r}`
+                    } else if ("card_req-link2" === n.className) {
+                        e.matches && t.preventDefault();
+                        const r = n.href;
+                        c.data = `${r}`
+                    } else if ("card_req-link3" === n.className) {
+                        e.matches && t.preventDefault();
+                        const r = n.href;
+                        a.data = `${r}`
+                    } else {
+                        if ("card_req-link4" !== n.className) return; {
+                            e.matches && t.preventDefault();
+                            const r = n.href;
+                            s.data = `${r}`
+                        }
+                    }
+                }))
+            }
+        }],
+        t = {};
 
-  function r(e, t) {
-    window.scrollY >= t ? e.classList.add("sticky") : e.classList.remove("sticky")
-  }
-  n.r(t), n.d(t, "menuSticky", (function () {
-    return r
-  }))
-}, function (e, t, n) {
-  "use strict";
-
-  function r() {
-    const e = document,
-      t = e.getElementById("slider"),
-      n = e.querySelector(".slider-prev"),
-      r = e.querySelector(".slider-next"),
-      c = e.querySelectorAll(".slider-item");
-    if (!t) return;
-    let i = 0,
-      o = c.length;
-
-    function l() {
-      c[i].classList.remove("active"), i++, i >= o && (i = 0), c[i].classList.add("active")
+    function n(r) {
+        var i = t[r];
+        if (void 0 !== i) return i.exports;
+        var c = t[r] = {
+            exports: {}
+        };
+        return e[r](c, c.exports, n), c.exports
     }
-    e.addEventListener("click", e => {
-      e.target === n && (e.preventDefault(), c[i].classList.remove("active"), i--, i < 0 && (i = o - 1), c[i].classList.add("active")), e.target === r && (e.preventDefault(), l())
-    }), setInterval(() => l(), 5e3)
-  }
-  n.r(t), n.d(t, "slider", (function () {
-    return r
-  }))
-}, function (e, t, n) {
-  "use strict";
-  n.r(t), n.d(t, "view_pdf", (function () {
-    return i
-  }));
-  const r = document,
-    c = r.getElementById("req");
-
-  function i() {
-    c && r.addEventListener("click", e => {
-      const t = e.target,
-        n = r.getElementById("file-view"),
-        c = r.getElementById("file-view2"),
-        i = r.getElementById("file-view3"),
-        o = r.getElementById("file-view4");
-      if ("card_req-link" === t.className) {
-        e.preventDefault();
-        const r = t.href;
-        n.src = "".concat(r)
-      } else if ("card_req-link2" === t.className) {
-        e.preventDefault();
-        const n = t.href;
-        c.src = "".concat(n)
-      } else if ("card_req-link3" === t.className) {
-        e.preventDefault();
-        const n = t.href;
-        i.src = "".concat(n)
-      } else {
-        if ("card_req-link4" !== t.className) return; {
-          e.preventDefault();
-          const n = t.href;
-          o.src = "".concat(n)
-        }
-      }
-    })
-  }
-}]);
+    n.d = function (e, t) {
+        for (var r in t) n.o(t, r) && !n.o(e, r) && Object.defineProperty(e, r, {
+            enumerable: !0,
+            get: t[r]
+        })
+    }, n.o = function (e, t) {
+        return Object.prototype.hasOwnProperty.call(e, t)
+    }, n.r = function (e) {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+            value: "Module"
+        }), Object.defineProperty(e, "__esModule", {
+            value: !0
+        })
+    };
+    var r = {};
+    ! function () {
+        n.r(r);
+        var e = n(1),
+            t = n(2),
+            i = n(3),
+            c = n(4);
+        const a = document,
+            s = window,
+            o = a.getElementById("nav-main"),
+            l = o.offsetTop;
+        a.addEventListener("DOMContentLoaded", (t => {
+            (0, e.default)(".icon-menu", ".menu"), (0, i.slider)(), (0, c.view_pdf)()
+        })), s.addEventListener("scroll", (e => {
+            (0, t.menuSticky)(o, l)
+        }))
+    }()
+}();
